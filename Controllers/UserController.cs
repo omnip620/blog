058f5a07fddceca65d7a1f8a87be33d5bc7986b3
p412726700 <412726700@qq.com>
2014-02-19 20:37:45 +0800

    start

---
+++ Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult HomeHead()
+        {
+            return View();
+        }
+
+        public ActionResult sideBarHotArticle()
+        {
+            return View();
+        }
+
+        public ActionResult sideBarLatestArticle()
+        {
+            return View();
+        }
+
+        public ActionResult sideBarHotLabels()
+        {
+            return View();
+        }
+
+        public ActionResult sideBarTimeStamp()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Scripts/index.js
@@ -0,0 +1,28 @@
+﻿var App = function () {
+
+    var winWidth = window.innerWidth;
+    var winHeight = window.innerHeight;
+    var doc = $(document);
+    var header = function () {
+        var added;
+        doc.scroll(function (e) {
+            if (doc.scrollTop() > 5) {
+                if (added) return;
+                added = true;
+                $('header').addClass('header');
+            } else {
+                $('header').removeClass('header');
+                added = false;
+            }
+        })
+
+    }
+
+    return {
+        init: function () {
+            header();
+        }
+
+    }
+
+}()
