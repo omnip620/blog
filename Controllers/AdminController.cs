bad692da541a81b0c382207a47fbb47d1649bd0d
p412726700 <412726700@qq.com>
2014-02-27 08:48:23 +0800

    admin_1

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AdminHeader()
+        {
+
+            //ViewData["userName"] = Request.Cookies.Get("userName").Value;
+            ViewData["userName"] = "PZC";
+            return View();
+
+        }
+
+        public ActionResult Articles()
+        {
+            return View();
+        }
+
+        public ActionResult AddAtricle()
+        { return View(); }
+
+
+
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -23,16 +23,28 @@ namespace blog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
-           "NoController", // 路由名称
-           "{action}/{id}", // 带有参数的 URL
-           new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
-            );
+"Admin", // 路由名称
+"Admin/{action}/{id}", // 带有参数的 URL
+new { controller = "Admin", action = "Index", id = UrlParameter.Optional } // 参数默认值
+);
+            routes.MapRoute(
+         "NoController", // 路由名称
+         "{action}/{id}", // 带有参数的 URL
+         new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
+          );
+
 
             routes.MapRoute(
-                "Default", // 路由名称
-                "{controller}/{action}/{id}", // 带有参数的 URL
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
-            );
+             "Default", // 路由名称
+             "{controller}/{action}/{id}", // 带有参数的 URL
+             new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
+         );
+
+
+
+
+
+
 
 
         }
